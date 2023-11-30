f7d41a7d00377c029a19e91bdc013441cc7171d7
izelozarslan <izel.ozarslan@gmail.com>
2023-12-01 00:03:26 +0300

    status field added

--- library-management-system/Book.cs
+++ library-management-system/Book.cs
@@ -2,15 +2,18 @@ namespace library_management_system;
 
 public class Book
 {
-    private string Id { get; }
-    private string Name { get; set; }
-    private string Author { get; set; }
-    private string PublicationYear { get; set; }
+    internal string Id { get; set; }
+    internal string Name { get; set; }
+    internal string Author { get; set; }
+    internal string PublicationYear { get; set; }
+    internal Status Status { get; set; }
 
-    public Book(string name, string author, string publicationYear)
+    public Book(string name, string author, string publicationYear, Status status)
     {
         Name = name;
         Author = author;
         PublicationYear = publicationYear;
+        Status = status;
     }
+    
 }
